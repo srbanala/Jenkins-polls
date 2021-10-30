abb4584808046bf23386c3cc9107bfdac9c51bc6
narsimhareddy akiti <srbanala13@gmail.com>
2021-10-30 15:45:05 -0400

    urls.py file

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,5 @@ from django.urls import include,path
 
 urlpatterns = [
     path('polls/',include('polls.urls')),
-    path('mortgage/',include('mortgage.urls')),
     path('admin/', admin.site.urls),
 ]
