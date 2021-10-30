4de64fc3d6db8d738b7095e620c6889adcbec54c
narsimhareddy akiti <srbanala13@gmail.com>
2021-10-29 22:05:50 -0400

    mysite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'w15^xuz%3d)34b46#bdwr+muu9wt-*%1qc+pv&h5farun3o0om'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['a3585abbdbbb64b779ae052651cd08f2-fe8981788354cac6.elb.us-east-1.amazonaws.com',]
-#ALLOWED_HOSTS=[]
+#ALLOWED_HOSTS = ['a3585abbdbbb64b779ae052651cd08f2-fe8981788354cac6.elb.us-east-1.amazonaws.com',]
+ALLOWED_HOSTS=[]
 
 # Application definition
 
@@ -75,26 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
- #   }
-#}
-
-
 DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'postgres',
-	   'USER': 'postgres',
-	   'PASSWORD':'welcome1',
-	    #'HOST':'127.0.0.1',
-       'HOST':'postgres-db-cluster-ip-service',
-        #'HOST':'django-polls1.chec4zibv9si.us-east-1.rds.amazonaws.com',
-	'PORT':'5432',
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
