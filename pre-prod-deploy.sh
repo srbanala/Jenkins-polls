c45ec207b383d057199c44de550ade6f5d4e8e0f
narsimhareddy akiti <srbanala13@gmail.com>
2021-11-10 09:41:15 -0500

    prod.sh

--- pre-prod-deploy.sh
+++ pre-prod-deploy.sh
@@ -1,4 +1,4 @@
-ssh ec2-user@10.0.102.109 -i /tmp/mykp.pem
+ssh -tt ec2-user@10.0.102.109 -i /tmp/mykp.pem
 sudo yum install docker
 sudo systemctl start docker
 sudo docker run -d -p 8000:8000 -t anreddy/polls_sqlite
