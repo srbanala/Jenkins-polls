ba71677a10f5b2a7094707d6a91582fec2a6146a
narsimhareddy akiti <srbanala13@gmail.com>
2021-11-10 11:41:24 -0500

    prod.sh

--- pre-prod-deploy.sh
+++ pre-prod-deploy.sh
@@ -1,5 +1,5 @@
 ssh  -tt ec2-user@10.0.102.109 -i /tmp/mykp.pem
-sudo yum install docker
-sudo systemctl start docker
-sudo docker run -d -p 8000:8000 -t anreddy/polls_sqlite
-sudo echo "docker process is running"
+ yum install docker
+ systemctl start docker
+ docker run -d -p 8000:8000 -t anreddy/polls_sqlite
+ echo "docker process is running"
