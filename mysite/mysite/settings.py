bf7b5229b6c266d66d688a5810720cb7d0093caa
narsimhareddy akiti <srbanala13@gmail.com>
2021-11-16 13:59:53 -0500

    settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w15^xuz%3d)34b46#bdwr+muu9wt-*%1qc+pv&h5farun3o0om'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myalb-925884056.us-east-1.elb.amazonaws.com','ec2-54-80-107-127.compute-1.amazonaws.com',]
+ALLOWED_HOSTS = ['myelb-1137503135.us-east-1.elb.amazonaws.com','ec2-54-158-31-110.compute-1.amazonaws.com',]
 #ALLOWED_HOSTS=[]
 
 # Application definition
