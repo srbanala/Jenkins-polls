30d9ba3bc5cd0d690978794d6d2aba10492e93b7
narsimhareddy akiti <srbanala13@gmail.com>
2021-11-22 09:28:48 -0500

    deploy

--- pre-prod-deploy.sh
+++ pre-prod-deploy.sh
@@ -1,5 +1,6 @@
- ssh  ec2-user@10.0.2.14 -i /tmp/mykp.pem
+ cat <<'EOT' ssh   ec2-user@10.0.2.14 -i /tmp/mykp.pem
  yum install docker
  systemctl start docker
  docker run -d -p 8000:8000 -t anreddy/polls_sqlite
  echo "docker process is running"
+ EOT
