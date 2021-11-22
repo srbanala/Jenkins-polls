c9bfc6eaa58c1979c6017ccd7339548520cea962
narsimhareddy akiti <srbanala13@gmail.com>
2021-11-22 10:48:47 -0500

    prod deploy

--- pre-prod-deploy.sh
+++ pre-prod-deploy.sh
@@ -1,4 +1,5 @@
- yum install docker
- systemctl start docker
- docker run -d -p 8000:8000 -t anreddy/polls_sqlite
+ sudo yum install docker
+ sudo systemctl start docker
+  sudo docker run -d -p 8000:8000 -t anreddy/polls_sqlite
+sudo docker ps
  echo "docker process is running"
