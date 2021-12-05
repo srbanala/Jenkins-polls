6edf950a5ccd656e00705ed38775697cdc1221d4
narsimhareddy akiti <srbanala13@gmail.com>
2021-12-05 15:56:43 -0500

    jenkins

--- pre-prod-deploy.sh
+++ pre-prod-deploy.sh
@@ -1,5 +1,5 @@
  sudo yum install docker -y
  sudo systemctl start docker
-  sudo docker run -d -p 8000:8000 -t anreddy/polls_sqlite
-sudo docker ps
+ sudo docker run --name polls_sqlite -d -p 8000:8000 -t anreddy/polls_sqlite
+ sudo docker ps
  echo "docker process is running"
